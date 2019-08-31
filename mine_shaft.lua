816bca2fac3b1bf746d3ae4dfd224a5ec636c136
rdepaiva <rdepaiva@envysion.com>
2019-08-30 23:40:40 -0700

    Added mineshaft and spruceGrab

---
+++ mine_shaft.lua
@@ -0,0 +1,90 @@
+os.loadAPI("/ComputerCraft_Learning/move.lua")
+if peripheral.isPresent("Left") and peripheral.getType("Left") == "modem" then
+    rednet.open("Left")
+    print("Openned Wifi on the left side")
+    wifi=true
+end
+channel=2
+turtle.select(1)
+
+params = { ... }
+if #params < 1 then
+    print("Usage: mine_shaft <distance>")
+    return
+end
+
+function caveWalkDig(Qtt)
+    for x=1, Qtt do
+        if not move.fd(1) then
+            turtle.dig()
+            move.fd(1)
+            digUpDown()
+        else
+            -- break up and down
+            digUpDown()
+        end
+    end
+end
+
+function shaft(howfar)
+    for x=1, howfar do
+        caveWalkDig(1)
+        -- place torches every 14
+        if x == 1 then
+            placeTorch()
+        end
+        if x % 14 == 0 then
+            placeTorch()
+        end
+    end
+    placeTorch()
+end
+
+function digUpDown()
+    while turtle.detectUp() or turtle.detectDown() do
+        turtle.digUp()
+        turtle.digDown()
+    end
+end
+
+function placeTorch()
+    turtle.select(11)
+    turtle.placeDown()
+    turtle.select(1)
+    if wifi == true then
+        rednet.send(channel, "I'm placing a torch", "status")
+    end
+end
+
+totalMove=tonumber(params[1])
+side=params[2]
+
+if wifi == true then
+    rednet.send(channel, "Starting the work, shaft with "..totalMove.." and turning to the "..side, "status")
+end
+
+shaft(totalMove)
+-- turn and get ready to go back, from other shaft
+if wifi == true then
+    rednet.send(channel, "I'm turning back", "status")
+end
+if side == "r" then
+    turtle.turnRight()
+else
+    turtle.turnLeft()
+end
+caveWalkDig(4)
+if side == "r" then
+    turtle.turnRight()
+else
+    turtle.turnLeft()
+end
+-- dig the next shaft back
+shaft(totalMove)
+-- Finishing and getting out of the way
+if side == "right" then
+    turtle.turnRight()
+else
+    turtle.turnLeft()
+end
+move.fd(1)
---
+++ spruceGrab.lua
@@ -0,0 +1,49 @@
+os.loadAPI("/ComputerCraft_Learning/move.lua")
+if peripheral.isPresent("Left") and peripheral.getType("Left") == "modem" then
+    rednet.open("Left")
+    print("Openned Wifi on the left side")
+    wifi=true
+end
+channel=2
+
+function cutit()
+    turtle.select(1)
+    turtle.dig()
+    move.fd(1)
+    while turtle.compareUp() do
+        turtle.digUp()
+        turtle.dig()
+        move.up(1)
+    end
+    turtle.turnRight()
+    turtle.dig()
+    move.fd(1)
+    turtle.turnLeft()
+    while turtle.compareDown() do
+        turtle.digDown()
+        turtle.dig()
+        move.dn(1)
+    end
+    turtle.dig()
+    move.bk(1)
+    turtle.turnLeft()
+    move.fd(1)
+    turtle.turnRight()
+end
+
+function plant()
+    turtle.select(15)
+    move.fd(1)
+    turtle.place()
+    turtle.turnRight()
+    move.fd(1)
+    turtle.turnLeft()
+    turtle.place()
+    turtle.turnRight()
+    move.bk(1)
+    turtle.place()
+    turtle.turnLeft()
+    move.bk(1)
+    turtle.place()
+end
+plant()
